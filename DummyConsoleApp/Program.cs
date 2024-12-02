d4d4119ca11dba93c2f39a67531b6cc4b37b3ec7
= <cuan.mcleman@gmail.com>
2024-12-02 17:05:22 +1100

    Add project files.

---
+++ DummyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using DummyConsoleApp;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Simulating hit");
+BlackJackSimulator.SimulateTests(100000, true, false);
+Console.WriteLine();
+Console.WriteLine("Simulating stand");
+BlackJackSimulator.SimulateTests(100000, false, false);
+
+//await new PrisonerProblemHandler(prisonerCount: 10, maxChainLength: 10000000, minChainLength: 7, logLevel:0).ProcessManyAsync(100000, 20);
