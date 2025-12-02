62418e530a8f20af27cd37f5d5d85cc5ed17c3f8
Cuan <cuan.mcleman@gmail.com>
2025-12-02 14:44:31 +1100

    Advent Of Code stuff

--- DummyConsoleApp/Program.cs
+++ DummyConsoleApp/Program.cs
@@ -1,11 +1,5 @@
-﻿
-using DummyConsoleApp;
+﻿using DummyConsoleApp.AdventOfCoding.Advent2025;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Simulating hit");
-BlackJackSimulator.SimulateTests(100000, true, false);
-Console.WriteLine();
-Console.WriteLine("Simulating stand");
-BlackJackSimulator.SimulateTests(100000, false, false);
 
-//await new PrisonerProblemHandler(prisonerCount: 10, maxChainLength: 10000000, minChainLength: 7, logLevel:0).ProcessManyAsync(100000, 20);
+Day1PasswordCracker solution = new();
+solution.Main();
