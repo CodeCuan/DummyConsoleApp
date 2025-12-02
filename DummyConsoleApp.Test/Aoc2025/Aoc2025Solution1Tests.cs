f71114b5dcd4fe6e60f1e9adaa008d434e649673
Cuan <cuan.mcleman@gmail.com>
2025-12-02 17:24:49 +1100

    refactored tests

--- DummyConsoleApp.Test/Aoc2025/Aoc2025Solution1Tests.cs
+++ DummyConsoleApp.Test/Aoc2025/Aoc2025Solution1Tests.cs
@@ -1,10 +1,5 @@
 ﻿using DummyConsoleApp.AdventOfCoding.Advent2025;
 using DummyConsoleApp.AdventOfCoding.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DummyConsoleApp.Test.Aoc2025;
 
@@ -15,37 +10,13 @@ public class Aoc2025Solution1Tests
         sut = new Day1PasswordCracker();
     }
 
-    [Fact]
-    public void TestCrackPasswords()
+    [Theory]
+    [InlineData("R1000", 10)]
+    [InlineData("L68\r\nL30\r\nR48\r\nL5\r\nR60\r\nL55\r\nL1\r\nL99\r\nR14\r\nL82",6)]
+    public void CrackPasswords_WithOfficialSample(string sampleData, int expectedCount)
     {
-        var sampleData = new List<string>
-        {
-            "R1000"
-        };
-
-        TestAndAssert(sampleData, 10);
-
-    }
-
-    [Fact]
-    public void TestCrackedPasswordsSample() {
-        var sampleData = @"L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
         var data = DataParser.SplitDataLine(sampleData, true);
-        TestAndAssert(data, 6);
-    }
-
-    private void TestAndAssert(List<string> data, int expectedValue) {
         var value = sut.CrackPasswordsAdvanced(data);
-
-        Assert.Equal(expectedValue, value);
+        Assert.Equal(expectedCount, value);
     }
 }
